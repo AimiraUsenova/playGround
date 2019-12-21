99058307812b8043d0ae2eb452929912649de285
aimirausenova <ajmirau@gmail.com>
2019-12-21 10:15:46 -0600

    Hello it's me Aimira

--- src/main/java/Slides.java
+++ src/main/java/Slides.java
@@ -2,7 +2,7 @@ public class Slides {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello it's me Aimira...");
+        System.out.println("Hello it's me Aimira....");
     }
 
 
